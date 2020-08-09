9cae640cab18da22446dd25c265e7574b0654637
Kieu Ngoc Trong <trong.kieu@infodation.local>
2020-08-09 23:32:53 +0700

    trong commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+
+  arrays = ['a','b','c','d','e'];
+
+  addMoreItem(){
+    this.arrays.push('f');
+  }
+
+  deleteItem(){
+    this.arrays.pop();
+  }
 }
