aa487ca26abd95b0a824310225192edd9602146e
Kieu Ngoc Trong <trong.kieu@infodation.local>
2020-08-31 13:48:50 +0700

    trong commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
+import { SideNavToggleComponent } from './side-nav-toggle/side-nav-toggle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavToggleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/side-nav-toggle/side-nav-toggle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-nav-toggle',
+  templateUrl: './side-nav-toggle.component.html',
+  styleUrls: ['./side-nav-toggle.component.css']
+})
+export class SideNavToggleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
