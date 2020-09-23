afa1790f4e56e5f6242533f82b57fcceb423570d
Kieu Ngoc Trong <trong.kieu@infodation.local>
2020-09-23 08:55:16 +0700

    trong commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular';
+
+  @ViewChild('vc', {read : ViewContainerRef}) vc: ViewContainerRef;
+  @ViewChild('tpl') tpl: TemplateRef<any>;
+
+  ngAfterViewInit(): void {
+    console.log(this.tpl.elementRef);
+  }
+
+  insertView(){
+    const view = this.tpl.createEmbeddedView(null);
+    this.vc.insert(view);
+  }
+
+  deleteView(){
+    this.vc.detach(0);
+  }
 }
