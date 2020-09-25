507a5184fbbcceb90ac9580a24a797b5a5d2fb00
Kieu Ngoc Trong <trong.kieu@infodation.local>
2020-09-25 17:42:12 +0700

    trong commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit {
+  constructor(public breakpointObserver: BreakpointObserver){}
+
+  ngOnInit() {
+    this.breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge
+    ]).subscribe((state: BreakpointState) => {
+      if (state.breakpoints[Breakpoints.XSmall]){
+        console.log('%cMatches XSmall viewport', 'color: blue');
+      }
+      if (state.breakpoints[Breakpoints.Small]) {
+        console.log('%cMatches Small viewport', 'color: blue');
+      }
+      if (state.breakpoints[Breakpoints.Medium]) {
+        console.log('%cMatches Medium  viewport', 'color: blue');
+      }
+      if (state.breakpoints[Breakpoints.Large]) {
+        console.log('%cMatches Large viewport', 'color: blue');
+      }
+      if (state.breakpoints[Breakpoints.XLarge]) {
+        console.log('%cMatches XLarge viewport', 'color: blue');
+      }
+    });
+
+
+    this.breakpointObserver.observe(['(min-width: 900px)']).subscribe((state: BreakpointState) => {
+      if (state.matches){
+        console.log('Viewport is 900px or over!');
+      } else {
+        console.log('Viewport is smaller than 900px!');
+      }
+    });
+
+    if (this.breakpointObserver.isMatched('(min-height: 900px)')) {
+      console.log('The 900px viewport matched!');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { LayoutModule } from '@angular/cdk/layout';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
