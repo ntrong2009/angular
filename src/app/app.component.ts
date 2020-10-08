66e66dd1d6faf191a6e83611673d1bdf9f45d191
Kieu Ngoc Trong <trong.kieu@infodation.local>
2020-10-08 09:27:05 +0700

    trong commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  OnInit,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
 
+
+// tslint:disable-next-line:no-conflicting-lifecycle
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+// tslint:disable-next-line:max-line-length
+export class AppComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
   title = 'angular';
+
+  constructor() {
+    alert('constructor');
+  }
+
+  ngOnChanges() {
+    alert('ngOnChanges');
+  }
+
+  ngOnInit() {
+    alert('ngOnInit');
+  }
+
+  ngDoCheck() {
+    alert('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    alert('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    alert('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    alert('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    alert('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    alert('ngOnDestroy');
+  }
 }
