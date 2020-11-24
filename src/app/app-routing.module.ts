7dbd9445cbb52ebcf90eb8fdee9c9c545fa10fb7
ngoc trong <ngoctrong437@gmail.com>
2020-11-24 14:19:27 +0700

    trong commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { DogComponent } from './dog/dog.component';
+import { CatComponent } from './cat/cat.component';
+import { DefaultComponent } from './default/default.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultComponent
+  },
+  {
+    path: 'dog',
+    component: DogComponent,
+  },
+  {
+    path: 'cat',
+    component: CatComponent
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DogComponent } from './dog/dog.component';
+import { CatComponent } from './cat/cat.component';
+import { DefaultComponent } from './default/default.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DogComponent,
+    CatComponent,
+    DefaultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
