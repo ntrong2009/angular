6ca628b0585bca7990a197ae80fde3d60d8a5e36
ngoc trong <ngoctrong437@gmail.com>
2021-01-12 22:19:36 +0700

    trong commit

--- src/app/product.actions.ts
+++ src/app/product.actions.ts
@@ -3,5 +3,5 @@ import { createAction, props } from '@ngrx/store';
 import { ProductModel } from 'src/app/product.model';
 
 export const loadProduct = createAction('load_product', props<{ payload: object }>() );
-console.log('load_product', typeof loadProduct);
-export const loadProductSuccess = createAction('load_product_success', props<{ payload: ProductModel }>())
+console.log('%c%s', 'color: #00bf00', 'loadProduct', loadProduct);
+export const loadProductSuccess = createAction('load_product_success', props<{ payload: ProductModel }>());
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,6 +11,6 @@ export class CounterService {
   ) { }
 
   getData(){
-    return this.http.get('http://localhost:3000/profile');
+    return this.http.get('http://localhost:1997/profile');
   }
 }
