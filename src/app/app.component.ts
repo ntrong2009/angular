76f86667e9c1387d8162efff24450c982a5890ec
ngoctrong <ngoctrong437@gmail.com>
2021-03-11 18:16:23 +0700

    trong commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit {
+  username;
+  formData;
+  constructor() {}
+
+  ngOnInit() {
+    this.formData = new FormGroup({
+      username: new FormControl('ngoctrong'),
+      pwd: new FormControl('123')
+    });
+  }
+
+  onClickSubmit(data) {
+    this.username = data.username;
+  }
 }
