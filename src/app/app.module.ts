9b11a040f4c86d98767d25ffd5152b6706e0a876
ngoctrong <ngoctrong437@gmail.com>
2021-05-25 14:35:24 +0700

    trong commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HostContextComponent } from './host-context/host-context.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HostContextComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/host-context/host-context.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-host-context',
+  templateUrl: './host-context.component.html',
+  styleUrls: ['./host-context.component.scss']
+})
+export class HostContextComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
