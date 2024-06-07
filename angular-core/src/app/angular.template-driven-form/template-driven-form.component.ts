a4b92f1dd1ae5c36860ad8a7f184bc3d495e79e7
TrongNgocKieu <ngoctrong437@gmail.com>
2024-06-07 10:03:06 +0700

    learn template driven form

---
+++ angular-core/src/app/angular.template-driven-form/template-driven-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component ({
+    selector: 'app-template-driven-form',
+    templateUrl: './template-driven-form.component.html',
+    styleUrls: ['./template-driven-form.component.scss']
+})
+export class TemplateDrivenForm implements OnInit {
+
+    constructor() {
+        
+    }
+
+    ngOnInit(): void {
+    }
+
+    onSubmit() {
+
+    }
+}
